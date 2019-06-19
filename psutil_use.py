c912003e11ffd3dcaca90cf2d967d14675ecc9ab
fanxu <fanxu@17shanyuan.com>
2019-06-19 11:38:53 +0800

    test

--- psutil_use.py
+++ psutil_use.py
@@ -1,19 +1,23 @@
 import psutil
 
 mm = list()
+# 目标执行进程启动文件名
+aim_file_name = []
 
 for proc in psutil.process_iter():
     if "python" in proc.name():
         print("pid:{},name{}".format(proc.pid, proc.name()))
-        mm.append(proc.pid)
-print(mm)
+        pp = psutil.Process(proc.pid)
+        p_status = pp.status()
+        mm = pp.cmdline()
+        print(mm)
+        use_comd = mm[-1].split("/")[-1]
+        print("启动文件：{},进程状态：{}".format(use_comd, p_status))
+# print(mm)
+#
+#
+# for m in mm:
 
-
-for m in mm:
-    pp = psutil.Process(m)
-    mm = pp.cmdline()
-    use_comd = mm[-1].split("/")[-1]
-    print(use_comd)
 
 
 
